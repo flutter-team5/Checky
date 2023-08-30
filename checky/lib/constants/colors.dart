52a24d87d57a18e4603a13d82b20cf8b89b61263
dev one <dev@test.com>
2023-08-30 11:26:59 +0300

    Add Spacing & Colors files

---
+++ checky/lib/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CColors {
+  static const primary = Color(0xFF060047); //FFDD83
+  static const darkPink = Color(0xFFB3005E);
+  static const brightPink = Color(0xFFE90064);
+  static const red = Color(0xFFEA5455);
+  static const ligthRed = Color.fromARGB(255, 234, 127, 127);
+  static const beige = Color(0xFFE4DCCF);
+  static const yellow = Color(0xFFFFDD83);
+  static const lightYellow = Color.fromARGB(255, 255, 229, 158);
+  static const lightBeige = Color(0xFFF9F5EB);
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const grey = Colors.grey;
+}
