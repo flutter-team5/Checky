03daffe9092e5e1183691d0b3d24a28e0957efd2
dev one <dev@test.com>
2023-09-08 23:00:57 +0300

    Finishing auth pages UI, Edite create assg page,Create controllers model

--- checky/lib/screens/assignments_views/create_assigment.dart
+++ checky/lib/screens/assignments_views/create_assigment.dart
@@ -1,6 +1,12 @@
+import 'package:checky/constants/colors.dart';
+import 'package:checky/constants/spacings.dart';
+import 'package:checky/model/controllers_model.dart';
+import 'package:checky/widgets/create_assg_widgets/test_cases_fileds.dart';
+import 'package:checky/widgets/labeld_text_field.dart';
 import 'package:flutter/material.dart';
 
-List testCase = ["0"];
+List<TestCaseFields> casesFieldList = [const TestCaseFields()];
+List<Controller> casesControllerList = [];
 
 class CreateAssigment extends StatefulWidget {
   const CreateAssigment({super.key});
@@ -10,140 +16,106 @@ class CreateAssigment extends StatefulWidget {
 }
 
 class _CreateAssigmentState extends State<CreateAssigment> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50), color: Colors.amber),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Align(
-            child: Text(
-              "create assigment",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            width: 300,
-            height: 50,
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "title",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+      body: Container(
+        margin: const EdgeInsets.only(
+          left: 25,
+          right: 25,
+          bottom: 50,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              // padding: const EdgeInsets.only(top: 60),
+              margin: const EdgeInsets.only(
+                top: 80,
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/img/logo_inside.png",
+                    width: 35,
+                  ),
+                  CSpaces.kHspace8,
+                  const Text(
+                    "Create new assigment",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            width: 300,
-            // height: 100,
-            child: TextField(
-              maxLines: 5,
-              decoration: InputDecoration(
-                labelText: "Desciption",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+            CSpaces.kVspace32,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TitledField(
+                    controller: titleController,
+                    label: "Title",
+                    hintText: "Enter assignment title",
+                    fontSize: 18,
+                    labelWeight: FontWeight.bold,
+                  ),
+                  CSpaces.kVspace24,
+                  TitledField(
+                    controller: titleController,
+                    label: "Description",
+                    hintText: "Enter assignment description",
+                    fieldMaxLines: 10,
+                    fontSize: 18,
+                    labelWeight: FontWeight.bold,
+                  ),
+                  CSpaces.kVspace24,
+                  const Text(
+                    "Test Cases",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  CSpaces.kVspace16,
+                  Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.only(top: 2),
+                      itemCount: casesFieldList.length,
+                      itemBuilder: (context, index) {
+                        return const TestCaseFields();
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            "Test Case",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return TestCaseConstructor();
-              },
-              itemCount: 3,
-
-              //  TestCaseConstructor(),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        backgroundColor: CColors.ligthRed,
+        onPressed: () {
+          if (casesFieldList.length < 3) {
+            casesFieldList.add(const TestCaseFields());
+          } else {
+            const snackBar = SnackBar(
+              content: Text('You reached the limit number of cases'),
+              duration: Duration(milliseconds: 1900),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            ;
+          }
+          setState(() {});
+        },
+        child: const Icon(Icons.add),
       ),
-    );
-  }
-}
-
-class TestCaseConstructor extends StatelessWidget {
-  const TestCaseConstructor({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: 100,
-          height: 50,
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: "input",
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 5,
-        ),
-        SizedBox(
-          width: 100,
-          height: 50,
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: "output",
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 5,
-        ),
-        SizedBox(
-          width: 100,
-          height: 50,
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: "Mark",
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
