14fd7346294579b06d637890c3b32062ef9e4cb8
muhamed <onair10@hotmail.com>
2023-09-10 13:34:16 +0300

    added ai screen

--- checky/lib/main.dart
+++ checky/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:checky/screens/assignments_views/main_screen.dart';
+import 'package:checky/screens/onboarding_screen.dart';
 import 'package:checky/screens/pre_auth/login_screen.dart';
 import 'package:checky/services/database/database_connector.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +14,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LogInScreen(),
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
+        ),
+      ),
+      home: OnboardingScreen(),
     );
   }
 }
+
+const defaultInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(16)),
+  borderSide: BorderSide(
+    color: Color(0xFFDEE3F2),
+    width: 1,
+  ),
+);
