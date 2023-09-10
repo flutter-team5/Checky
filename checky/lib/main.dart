100270772187679a5fe18a015d795a831128d1d1
dev one <dev@test.com>
2023-09-10 10:57:31 +0300

    change the run widget

--- checky/lib/main.dart
+++ checky/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:checky/screens/assignments_views/single_assignment_screen.dart';
 import 'package:checky/screens/pre_auth/login_screen.dart';
 import 'package:checky/services/database/database_connector.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SingleAssignmentScreen(),
-      //LogInScreen(),
+      home: LogInScreen(),
     );
   }
 }
