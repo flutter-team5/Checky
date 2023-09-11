47ab96fd32454f35bc066b7beec2be58a0ee7394
ruba <robasaad14@gmail.com>
2023-09-11 11:23:59 +0300

    some fixes

--- checky/lib/screens/splash_screen.dart
+++ checky/lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:checky/screens/assignments_views/main_screen.dart';
+import 'package:checky/screens/pre_auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -10,19 +11,19 @@ class SplashScreen extends StatelessWidget {
     return MaterialApp(
         home: FlutterSplashScreen.gif(
             backgroundColor: Colors.white,
-            gifPath: 'assets/gifs/CHECKY.gif',
+            gifPath: 'assets/gifs/CHECKY(3).gif',
             gifWidth: MediaQuery.of(context).size.width,
             gifHeight: MediaQuery.of(context).size.width,
-            defaultNextScreen: const MainScreen(),
-            duration: const Duration(milliseconds: 3000),
+            defaultNextScreen: const LogInScreen(),
+            duration: const Duration(milliseconds: 2500),
             onInit: () async {
               debugPrint("onInit 1");
-              await Future.delayed(const Duration(milliseconds: 3000));
+              await Future.delayed(const Duration(milliseconds: 2500));
               debugPrint("onInit 2");
             },
             onEnd: () async {
               debugPrint("onEnd 1");
-              await Future.delayed(const Duration(milliseconds: 3100));
+              await Future.delayed(const Duration(milliseconds: 2500));
               debugPrint("onEnd 2");
             }));
   }
